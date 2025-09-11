e2076afd1b9c0bbd3922b0cad395e530707798df
studioshakur-ui <studio.shakur@gmail.com>
2025-09-11 20:26:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-   base: '/<ton-repo>/',
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    alias: { "@": path.resolve(__dirname, "src") },
   },
 });
