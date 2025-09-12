73aa35b6cffbd1c57db1cc110b0e0d1aa86de687
studioshakur-ui <studio.shakur@gmail.com>
2025-09-12 09:13:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",             // racine = dossier courant
-  base: "/",             // Netlify/host standard. (Pour GH Pages: '/nom-repo/')
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "src") },
-  },
+  root: ".",
+  base: "/",
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+  server: { host: "0.0.0.0", port: 5173 },
   build: {
     rollupOptions: {
-      // on force l'entrée sur le vrai index.html du projet
       input: path.resolve(__dirname, "index.html"),
     },
   },
